d6ff8060fd2b3835bd782031019ae7d1ce344611
flaviosp15 <flaviosp15@outlook.com>
2025-11-21 12:54:49 -0300

    fix: create logs directory automatically to prevent FileNotFoundError

--- server/index.py
+++ server/index.py
@@ -43,7 +43,6 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
         logging.StreamHandler(),
-        logging.FileHandler('logs/app.log') if settings.environment == "production" else logging.StreamHandler()
     ]
 )
 
